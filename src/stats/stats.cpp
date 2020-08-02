e2f4821d2bf1ed0eb88b4bf16f0b59369d64fe01
Kristian Kramer <kristian@beyondcoin.io>
2020-08-02 00:00:41 +0000

    Fix missing character

--- src/stats/stats.cpp
+++ src/stats/stats.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <stats/stats.h"
+#include <stats/stats.h>
 
 #include <memusage.h>
 #include <utiltime.h>
